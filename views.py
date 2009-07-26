89229c0c2e308142f56b98c56c2068d5503dbcee
Parag k. Shah <adaptives@gmail.com>
2009-07-26 12:25:50 +0530

    added about page

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import render_to_response
 from django.contrib.auth.forms import UserCreationForm
@@ -29,3 +30,6 @@ def register(request):
 		form = UserCreationForm()
 	return render_to_response("registration/register.html", {'form': form,})
 
+
+def about(request):
+	return render_to_response("about.html", context_instance=RequestContext(request))
