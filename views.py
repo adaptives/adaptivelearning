2e3dff58d299134800a31a5409324bbb760cb2e4
Parag k. Shah <adaptives@gmail.com>
2009-09-05 21:41:46 +0530

    site redesign and added themes

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from django.forms import ModelForm
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from django.template import loader, Context
 from django.shortcuts import render_to_response
 from django.shortcuts import render_to_response
 from django.contrib.auth.forms import UserCreationForm
@@ -57,3 +58,83 @@ def terms_of_service(request):
 
 def privacy_policy(request):
 	return render_to_response("privacy_policy.html", context_instance=RequestContext(request))
+
+def style(request, file_name):
+
+	from themes.skyish import theme
+
+	body = {'color':theme['body_fg'],
+					'background':theme['body_bg'],
+					'font_family':'''Georgia, "Times New Roman", serif, "Helvetica Neue", Helvetica, Arial, sans-serif'''}
+
+	header = {'background':theme['header_bg'] + ' none repeat scroll 0 0',
+						'border_bottom':'1px solid ' + theme['header_bottom_border'],
+						'nav_main__background_color':theme['nav_main_bg'],
+						'nav_main__color':theme['nav_main_fg'],
+						'nav_main__border_top':'1px solid ' + theme['nav_main_border_top'],
+						'nav_main__font_family':""""Helvetica Neue",Helvetica,Arial,sans-serif""",
+						'nav_main__a_visited_color':theme['nav_main_a_visited'],
+						'nav_main__a_link_color':theme['nav_main_a_link'],
+						'nav_main__a_hover_color':theme['nav_main_a_hover']}
+
+	h2 = {'color':theme['h2_color'],
+				'font_size':'1.3em',
+				'main_width':'70%',}
+
+	content = {'width':'1024px',
+						'main__width':'70%',
+						'sidebar__width':'27%',
+						'sidebar__min_height':'750px',
+						'sidebar__background_image':"url(\'/site-media/images/blueLinesGradient-1068x577.jpg\')",
+						'sidebar__a_hover_color':theme['sidebar_a_hover']}
+
+	footer = {'color':theme['footer_fg'],
+						'background_color':theme['footer_bg'],
+						'width':'1024px',
+						'a_link__color':theme['footer_a_link'],
+						'a_link__font_size':'11px',
+						'a_visited__color':theme['footer_a_visited'],
+						'a_visited__font_size':'11px',
+						'a_hover__color':theme['footer_a_hover'],
+						'a_hover__font_size':'11px'}
+
+	course_details = {'border_bottom':'1px dotted ' + theme['course_details_bottom_border']}
+
+	course_listing = {'a_link__color':theme['course_listing_a_link'],
+										'a_visited__color':theme['course_listing_a_visited'],
+										'a_hover__color':theme['course_listing_a_hover']}
+
+	course_name = {'color':theme['course_name']}
+	topic_name = {'color':theme['topic_name']}
+
+	course_description = {'a_visited__color':theme['course_description_a_visited'],
+												'a_link__color':theme['course_description_a_link'],
+												'a_hover__color':theme['course_description_a_hover']}
+
+	course_topics = {'a_link__color':theme['course_topics_a_link'],
+									'a_visited__color':theme['course_topics_a_visited'],
+									'a_hover__color':theme['course_topics_a_hover']}
+
+	link_theme_b = {'a_link__color':theme['link_theme_b_a_link'],
+									'a_visited__color':theme['link_theme_b_a_link'],
+									'a_hover__color':theme['link_theme_b_a_link']}
+
+	a = {'link_color':theme['a_link'],
+			 'visited_color':theme['a_visited']}
+
+	template = loader.get_template('style-django.tml')
+	context = Context({'body':body,
+										'header':header,
+										'content':content,
+										'h2':h2,
+										'footer':footer,
+										'course_details':course_details,
+										'course_listing':course_listing,
+										'course_name':course_name,
+										'topic_name':topic_name,
+										'course_description':course_description,
+										'course_topics':course_topics,
+										'link_theme_b':link_theme_b,
+										'a':a})
+	css = template.render(context)
+	return HttpResponse(css, mimetype="text/css")
